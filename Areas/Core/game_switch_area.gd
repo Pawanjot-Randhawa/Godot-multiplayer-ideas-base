83671726f8fd2980570a001d079ecb661a33b703
Pawanjot Randhawa <pawanjot2004@gmail.com>
2026-05-06 16:56:03 -0400

    Fixed circular area switcher dependency

--- Areas/Core/game_switch_area.gd
+++ Areas/Core/game_switch_area.gd
@@ -1,14 +1,16 @@
 extends Area3D
 
 
-@export var area_to_switch_to : PackedScene
+@export_file(".tscn") var target_area : String
+#Export file must be used as this scene can cause circular depencys where A loads B, and B loads A
+#With export file we can just load the scene when the player actually enters the area, avoiding the infinite loop
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player and multiplayer.is_server():
 		var authority = "SERVER" if multiplayer.is_server() else "CLIENT"
 		print(authority + " enter")
-		Signalbus.change_level_to.emit(area_to_switch_to)
+		Signalbus.change_level_to.emit(load(target_area))
 
 
 func _on_body_exited(body: Node3D) -> void:
